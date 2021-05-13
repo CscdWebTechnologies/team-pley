2bc3b00829f62921744ae6f37a2099322855d969
Fredrick Otabil <fredrickotabil97@gmail.com>
2021-05-13 23:56:53 +0000

    --created the footer--

---
+++ src/components/Footer.js
@@ -0,0 +1,474 @@
+import './src/Footer.css'
+const Footer = () => {
+  return (
+    <div className='Footer'>
+      <div className='Footer-wrapper'>
+        <div className='Footer-wrapper-grid'>
+          <div className='Footer-wrapper-grid-1'>
+            <div className='Footer-wrapper-grid-1-gaxxxx'>
+              <div className='Footer-wrapper-grid-1'>
+                <div className='Footer-wrapper-grid1-content'>
+                  <img
+                    src='https://images.ctfassets.net/q5ulk4bp65r7/3TBS4oVkD1ghowTqVQJlqj/2dfd4ea3b623a7c0d8deb2ff445dee9e/Consumer_Wordmark.svg'
+                    alt=''
+                    className='hit-me-now'
+                  />
+                  <select
+                    aria-label='Language selector'
+                    className='language-box'
+                  >
+                    <option value='de'>Deutsch</option>
+                    <option selected='' value='en'>
+                      English
+                    </option>
+                    <option value='es'>Español - España</option>
+                    <option value='es-LA'>Español - América Latina</option>
+                    <option value='fr'>Français - France</option>
+                    <option value='id'>Bahasa Indonesia</option>
+                    <option value='it'>Italiano</option>
+                    <option value='ko'>한국어</option>
+                    <option value='pl'>Polski</option>
+                    <option value='pt'>Português - Brasil</option>
+                    <option value='pt-PT'>Português - Portugal</option>
+                    <option value='ru'>Pусский</option>
+                    <option value='th'>ไทย</option>
+                    <option value='tr'>Türkçe</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className='heyman-bizzz-fdfdff'>
+                <div className='Copyright'>© 2021 Coinbase</div>
+                <div className='Flex-l69ttv-0 gaVUrq'>
+                  <a
+                    href='https://blog.coinbase.com/'
+                    title='The Coinbase Blog'
+                    className='ctoibS'
+                    color='slate'
+                    rel='noopener'
+                    target='_blank'
+                  >
+                    Blog
+                  </a>
+                  <span className='gqMQmO'>•</span>
+                  <a
+                    href='https://twitter.com/coinbase'
+                    title='Coinbase Twitter account'
+                    class='ctoibS'
+                    color='slate'
+                    rel='noopener'
+                    target='_blank'
+                  >
+                    Twitter
+                  </a>
+                  <span className='gqMQmO'>•</span>
+                  <a
+                    href='https://www.facebook.com/Coinbase'
+                    title='Coinbase Facebook page'
+                    className='ctoibS'
+                    color='slate'
+                    rel='noopener'
+                    target='_blank'
+                  >
+                    Facebook
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/**Footer grid 2 */}
+          <div class='eWjsKr'>
+            <div>
+              <div class='khROFT'>
+                <h2 class='FQIAq'>Company</h2>
+                <a
+                  href='/about'
+                  title='Learn more about the company'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  About
+                </a>
+                <a
+                  href='/careers'
+                  title='Work at Coinbase'
+                  class='ctoibS'
+                  color='slate'
+                  weight='inherit'
+                >
+                  Careers
+                </a>
+                <a
+                  href='/affiliates'
+                  title='Earn money becoming an affiliate'
+                  class='ctoibS'
+                  color='slate'
+                  weight='inherit'
+                >
+                  Affiliates
+                </a>
+                <a
+                  href='https://blog.coinbase.com/'
+                  title='The Coinbase Blog'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Blog
+                </a>
+                <a
+                  href='/press'
+                  title='In the news'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Press
+                </a>
+                <a
+                  href='https://investor.coinbase.com/'
+                  title='Investors'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Investors
+                </a>
+                <a
+                  href='/legal/user_agreement'
+                  title='Our privacy and legal policies'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Legal &amp; privacy
+                </a>
+                <a title='Cookie policy' class='ctoibS' color='slate'>
+                  Cookie policy
+                </a>
+              </div>
+              <div class='khROFT'>
+                <h2 class='Footer__LinkHeading-sc-1nr3vef-4 FQIAq'>Learn</h2>
+                <a
+                  href='/browse'
+                  title='Browse crypto prices'
+                  class='ctoibS'
+                  color='slate'
+                  weight='inherit'
+                >
+                  Browse crypto prices
+                </a>
+                <a
+                  href='/learn/crypto-basics'
+                  title='Crypto basics'
+                  class='ctoibS'
+                  color='slate'
+                  weight='inherit'
+                >
+                  Crypto basics
+                </a>
+                <a
+                  href='/learn/tips-and-tutorials'
+                  title='Tips &amp; tutorials'
+                  class='ctoibS'
+                  color='slate'
+                  weight='inherit'
+                >
+                  Tips &amp; tutorials
+                </a>
+                <a
+                  href='/learn/market-updates'
+                  title='Market updates'
+                  class='ctoibS'
+                  color='slate'
+                  weight='inherit'
+                >
+                  Market updates
+                </a>
+                <a
+                  href='/learn/crypto-basics/what-is-bitcoin'
+                  title='What is Bitcoin?'
+                  class='ctoibS'
+                  color='slate'
+                  weight='inherit'
+                >
+                  What is Bitcoin?
+                </a>
+                <a
+                  href='/learn/crypto-basics/what-is-cryptocurrency'
+                  title='What is crypto?'
+                  class='ctoibS'
+                  color='slate'
+                  weight='inherit'
+                >
+                  What is crypto?
+                </a>
+                <a
+                  href='/learn/crypto-basics/what-is-a-blockchain'
+                  title='What is a blockchain?'
+                  class='ctoibS'
+                  color='slate'
+                  weight='inherit'
+                >
+                  What is a blockchain?
+                </a>
+                <a
+                  href='/learn/tips-and-tutorials/how-to-set-up-a-crypto-wallet'
+                  title='How to set up a crypto wallet'
+                  class='ctoibS'
+                  color='slate'
+                  weight='inherit'
+                >
+                  How to set up a crypto wallet
+                </a>
+                <a
+                  href='/learn/tips-and-tutorials/how-to-send-crypto'
+                  title='How to send crypto'
+                  class='ctoibS'
+                  color='slate'
+                  weight='inherit'
+                >
+                  How to send crypto
+                </a>
+                <a
+                  href='/learn/tips-and-tutorials/crypto-and-bitcoin-taxes-US'
+                  title='2019 Crypto Tax Guide'
+                  class='ctoibS'
+                  color='slate'
+                  weight='inherit'
+                >
+                  Taxes
+                </a>
+              </div>
+            </div>
+            <div>
+              <div class='khROFT'>
+                <h2 class='Footer__LinkHeading-sc-1nr3vef-4 FQIAq'>
+                  Individuals
+                </h2>
+                <a
+                  href='/dashboard'
+                  title='Buy &amp; sell'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Buy &amp; sell
+                </a>
+                <a
+                  href='/earn'
+                  title='Earn free crypto'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Earn free crypto
+                </a>
+                <a
+                  href='https://wallet.coinbase.com'
+                  title='Coinbase Wallet - Store your cryptocurrency'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Wallet
+                </a>
+                <a
+                  href='https://card.coinbase.com/'
+                  title='Coinbase Card'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Card
+                </a>
+              </div>
+              <div class='khROFT'>
+                <h2 class='FQIAq'>Businesses</h2>
+                <a
+                  href='https://prime.coinbase.com'
+                  title='Cryptocurrency trading for institutions'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Prime
+                </a>
+                <a
+                  href='https://custody.coinbase.com'
+                  title='Coinbase Custody - Cryptocurrency custody for institutions'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Custody
+                </a>
+                <a
+                  href='https://assethub.coinbase.com'
+                  title='List your asset on Coinbase'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Asset Hub
+                </a>
+                <a
+                  href='https://commerce.coinbase.com'
+                  title='Coinbase Commerce - Start accepting cryptocurrency payments'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Commerce
+                </a>
+              </div>
+              <div class='khROFT'>
+                <h2 class='FQIAq'>Developers</h2>
+                <a
+                  href='https://cloud.coinbase.com'
+                  title='Coinbase Cloud'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Coinbase Cloud
+                </a>
+                <a
+                  href='https://developers.coinbase.com/docs/wallet/coinbase-connect'
+                  title='Connect'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Connect
+                </a>
+                <a
+                  href='https://commerce.coinbase.com/docs'
+                  title='Commerce'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Commerce
+                </a>
+                <a
+                  href='https://developers.coinbase.com/docs/exchange/'
+                  title='Pro'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Pro
+                </a>
+                <a
+                  href='https://bisontrails.co/'
+                  title='Bison Trails'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Bison Trails
+                </a>
+                <a
+                  href='https://walletlink.org/'
+                  title='WalletLink'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  WalletLink
+                </a>
+                <a
+                  href='https://www.rosetta-api.org/'
+                  title='Rosetta'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Rosetta
+                </a>
+                <a
+                  href='https://www.centre.io/developer-resources'
+                  title='USDC'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  USDC
+                </a>
+              </div>
+            </div>
+            <div>
+              <div class='khROFT'>
+                <h2 class='FQIAq'>Support</h2>
+                <a
+                  href='https://help.coinbase.com/'
+                  title='Our help center'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Help center
+                </a>
+                <a
+                  href='https://help.coinbase.com/en/contact-us'
+                  title='Contact us'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Contact us
+                </a>
+                <a
+                  href='https://help.coinbase.com/en/coinbase/getting-started/authentication-and-verification/create-a-coinbase-account'
+                  title='Coinbase support article for creating an account'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Create account
+                </a>
+                <a
+                  href='https://help.coinbase.com/en/coinbase/managing-my-account#identity-verification'
+                  title='Coinbase support article for ID verification'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  ID verification
+                </a>
+                <a
+                  href='https://help.coinbase.com/en/coinbase/managing-my-account#account-information'
+                  title='Coinbase support article for account information'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Account information
+                </a>
+                <a
+                  href='https://help.coinbase.com/en/coinbase/getting-started#adding-a-payment-method'
+                  title='Coinbase support article for payment methods'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Payment methods
+                </a>
+                <a
+                  href='https://help.coinbase.com/en/coinbase/managing-my-account#account-recovery'
+                  title='Coinbase support article for account access'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Account access
+                </a>
+                <a
+                  href='https://help.coinbase.com/en/coinbase/getting-started/general-crypto-education/supported-cryptocurrencies'
+                  title='Coinbase support article for supported cryptocurrencies'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Supported crypto
+                </a>
+                <a
+                  href='/places'
+                  title='Countries where Coinbase operates'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Supported countries
+                </a>
+                <a
+                  href='https://status.coinbase.com'
+                  title='Check the operational status of Coinbase services'
+                  class='ctoibS'
+                  color='slate'
+                >
+                  Status
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
