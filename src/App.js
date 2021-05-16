51e9b2636426e8d52951edc7405ade7e9383cbc4
Fredrick Otabil <fredrickotabil97@gmail.com>
2021-05-16 14:57:55 +0000

    worked on the crypto section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
-import Accounts from "./components/Accounts";
-import BlueBanner from "./components/BlueBanner";
-import Header from "./components/Header";
-import Landing from "./components/Landing";
-import Portfolio from "./components/Portfolio";
-import SpaceBanner from "./components/SpaceBanner";
-import Table from "./components/Table";
+import React from 'react'
+import Accounts from './components/Accounts'
+import BlueBanner from './components/BlueBanner'
+import Crypto from './components/Crypto'
+import Header from './components/Header'
+import Landing from './components/Landing'
+import Portfolio from './components/Portfolio'
+import SpaceBanner from './components/SpaceBanner'
+import Table from './components/Table'
 
 // The app component
 const App = () => {
@@ -15,11 +16,12 @@ const App = () => {
       <Landing />
       <Table />
       <Portfolio />
+      <Crypto />
       <BlueBanner />
       <Accounts />
       <SpaceBanner />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
